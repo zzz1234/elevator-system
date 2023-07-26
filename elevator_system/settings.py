1e01bf2c555765149309087e3ee073499d6d6cc9
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-07-26 23:32:41 +0530

    Adding Swagger file, elaborate READ.ME file for the project

--- elevator_system/settings.py
+++ elevator_system/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'elevator'
 ]
 
@@ -123,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django DRF Elevator System',
+}
